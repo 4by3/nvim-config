689e7729f31f360ef98cbb56a33c76fc74121515
Tim O <timothywastaken@hotmail.com>
2026-02-06 17:26:09 +1100

    Initial commit

---
+++ lua/config/options.lua
@@ -0,0 +1,36 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.opt.relativenumber = true
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.wrap = true
+
+vim.g.clipboard = {
+  name = "wl-clipboard",
+  copy = {
+    ["+"] = { "wl-copy", "--foreground", "--type", "text/plain" },
+    ["*"] = { "wl-copy", "--foreground", "--primary", "--type", "text/plain" },
+  },
+  paste = {
+    ["+"] = { "wl-paste", "--no-newline" },
+    ["*"] = { "wl-paste", "--no-newline", "--primary" },
+  },
+  cache_enabled = true,
+}
+
+vim.g.clipboard = {
+  name = "wl-clipboard",
+  copy = {
+    ["+"] = "wl-copy",
+    ["*"] = "wl-copy",
+  },
+  paste = {
+    ["+"] = "wl-paste",
+    ["*"] = "wl-paste",
+  },
+  cache_enabled = 1,
+}
+
+vim.opt.clipboard = "unnamedplus"
---
+++ lua/plugins/theme.lua
@@ -0,0 +1,1 @@
+/home/fbyt/.config/omarchy/current/theme/neovim.lua

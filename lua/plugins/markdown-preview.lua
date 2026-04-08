71c03825905df8fa06cb28c364286e54d11abe52
Tim O <timothywastaken@hotmail.com>
2026-04-08 21:12:22 +1000

    Made markdown hybrid

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -2,6 +2,17 @@
 return {
   "OXY2DEV/markview.nvim",
   lazy = false,
+  opts = {
+    hybrid_mode = true,
+    preview = {
+      -- This clears decorations on the current line/block
+      linewise_hybrid_mode = true,
+
+      -- Optional: specific modes where hybrid mode is active
+      -- Usually 'n' (normal) and 'i' (insert)
+      hybrid_modes = { "n", "i" },
+    },
+  },
 
   -- Completion for `blink.cmp`
   -- dependencies = { "saghen/blink.cmp" },

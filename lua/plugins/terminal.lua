e34bce05168a215433b325835a61975368316429
Tim O <timothywastaken@hotmail.com>
2026-04-17 22:40:24 +1000

    Removed unnecessary files

--- lua/plugins/terminal.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "akinsho/toggleterm.nvim",
-  opts = {
-    size = 50, -- Fixed small height so it doesn't "grow" visually
-    persist_size = false, -- No saving weird sizes
-    close_on_exit = "toggle", -- Ensures clean close on toggle
-    direction = "vertical",
-  },
-}
